3316d57a0571fdf0e7fed0cafe681125155072ea
IoannisTzotze <john.xoxe@gmail.com>
2023-06-15 19:12:10 +0100

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  setTimeout(function () {
+    $(".mask-container .color-image").addClass("mask-animation");
+  }, 200);
+
+  function hideBackground() {
+    $(".bw-image").css("display", "none");
+  }
+
+  setTimeout(hideBackground, 1500);
+});
